1d6ccce4f0b7aec6dcc60f8f8789299c4ef75e56
Matt Oross <orossm1@udayton.edu>
2018-09-10 11:33:08 -0400

    Initial Landing Page

---
+++ Benchmark/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Benchmark
+//
+//  Created by Elizabeth LoPresti on 9/10/18.
+//  Copyright © 2018 Benchmark Systems, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+}
